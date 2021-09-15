763758f6c0408b7c010d06dbe11c0469bc702719
Izyumets Aleksandr <izyumets.a@yandex.ru>
2021-09-15 20:31:59 +0300

    Initial Commit

---
+++ CalculationOfShapesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculationOfShapesApp
+//
+//  Created by Izyumets Aleksandr on 15.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
