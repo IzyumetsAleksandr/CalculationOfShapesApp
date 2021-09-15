d175161e9eabc7f267e8ccf565fdbe64173d1044
AndreyOvchinnikovDev <suetnoy@bk.ru>
2021-09-15 20:45:26 +0300

    commit2

---
+++ CalculationOfShapesApp/FigureModel.swift
@@ -0,0 +1,37 @@
+//
+//  FigureModel.swift
+//  CalculationOfShapesApp
+//
+//  Created by Andrey Ovchinnikov on 15.09.2021.
+//
+
+import Foundation
+
+struct Figure {
+    let side: Double
+    let width: Double
+    let height: Double
+    let length: Double
+    let radius: Double
+    
+    var volumeOfCube: Double {
+        pow(side, 3)
+    }
+    
+    var volumeOfCone: Double {
+        1 / 3 * 3.14 * pow(radius, 2) * height
+    }
+    
+    var volumeOfCylinder: Double {
+        3.14 * pow(radius, 2) * height
+    }
+    
+    var volumeOfSphere: Double {
+        4 / 3 * 3.14 * pow(radius, 3)
+    }
+    
+    var volumeOfRectangularPrism: Double {
+        width * height * length
+    }
+}
+
